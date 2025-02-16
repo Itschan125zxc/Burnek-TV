3f8ec8b48cb1d172c52fb40a647aac77f27f552a
Itschan125zxc <johnchristian.a.espillardo@gmail.com>
2025-02-16 18:31:51 +0800

    Create channel.js

---
+++ channel.js
@@ -0,0 +1,14 @@
+[
+  {
+    "name": "Channel 1",
+    "src": "https://example.com/stream1.m3u8",
+    "key": "1234567890abcdef:abcdef1234567890",
+    "drm": "clearKey"
+  },
+  {
+    "name": "Channel 2",
+    "src": "https://example.com/stream2.mpd",
+    "key": null,
+    "drm": ""
+  }
+]
